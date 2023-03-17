84e2448af9dc81cc467375500308c9b0d8c400e6
Edi Carlos <edi_csilva@hotmail.com>
2023-03-17 17:16:21 -0300

    configurado os arquivos para usar módulo do javascript

---
+++ controller/listaClientes-controller.js
@@ -0,0 +1,35 @@
+import { clienteService } from '../service/cliente-service.js';
+
+// gerando o template
+const criaNovaLinha = (nome, email) => {
+    const linhaNovoCliente = document.createElement('tr');
+
+    const conteudo = `
+        <td class="td" data-td>${nome}</td>
+        <td>${email}</td>
+        <td>
+            <ul class="tabela__botoes-controle">
+                <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+            </ul>
+        </td>
+                    `
+
+    linhaNovoCliente.innerHTML = conteudo;
+
+    return linhaNovoCliente;
+}
+
+// Percorrendo o DOM
+const tabela = document.querySelector('[data-tabela]');
+
+// fazendo junção com listaClientes que é uma promise
+clienteService.listaClientes()
+    .then(data => {
+        // console.log(data);
+        // const data = JSON.parse(http.response);
+
+        data.forEach(elemento => {
+            tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email));
+        });
+    })
